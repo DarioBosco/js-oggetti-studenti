bc7e76f2869251d8fa498cc86d09b8335e6e796a
Dario Bosco <dario_bosco@protonmail.com>
2021-07-30 14:37:46 +0200

    Esercizio completato

--- src/js/main.js
+++ src/js/main.js
@@ -1,20 +1,53 @@
 /* 
 //Creare un oggetto che descriva uno studente, con le seguenti proprietà: nome, cognome e età.
 //Stampare a schermo attraverso un ciclo for-in tutte le proprietà dell'oggetto.
-Creare un array di oggetti di studenti.
-Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
-Dare la possibilità all’utente, attraverso 3 prompt, di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
+//Creare un array di oggetti di studenti.
+//Ciclare su tutti gli studenti e stampare per ognuno di essi, nome e cognome.
+//Dare la possibilità all’utente, attraverso 3 prompt, di aggiungere un nuovo oggetto studente inserendo nell’ordine: nome, cognome e età.
  */
 
-//Creo il primo oggetto
-const student = {
+var userInputName = prompt('Inserisci il nome');
+var userInputSurname = prompt('Inserisci il cognome');
+var userInputAge = parseInt(prompt(`Inserisci l'eta'`));
+
+var student = {
 	name: 'Dario',
 	surname: 'Bosco',
 	age: 24,
 };
 
-console.log(student);
+var classRoom = [
+	{
+		name: 'Mario',
+		surname: 'Rossi',
+		age: 26,
+	},
+	{
+		name: 'Luigi',
+		surname: 'Verdi',
+		age: 26,
+	},
+	{
+		name: 'Gianluigi',
+		surname: 'Polenta',
+		age: 24,
+	},
+];
 
-for (key in student) {
-	console.log(student[key]);
+//Potrei implementare la prima lettera maiuscola ma non ho voglia
+classRoom.push({ name: userInputName, surname: userInputSurname, age: userInputAge });
+console.log(classRoom);
+
+for (const key in student) {
+	const element = student[key];
+	console.log(element);
+}
+
+for (let index = 0; index < classRoom.length; index++) {
+	const i = classRoom[index];
+
+	for (let key in i) {
+		const element = i[key];
+		console.log(element);
+	}
 }
